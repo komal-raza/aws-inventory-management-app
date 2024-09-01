9c54dc55b43c68c20f9d0aec436439bd6ecd4244
razaakomal <komal.raza@leoatech.com>
2024-09-01 18:58:22 +0500

    add config

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -31,8 +31,8 @@ app.use("/users", userRoutes);
 app.use("/expenses", expenseRoutes);
 
 /* Server */
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running at ${port} PORT`);
 });
